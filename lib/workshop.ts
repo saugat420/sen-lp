663c5c13b4e0f98de81225879c235e1fda894770
saugat420 <bsaugat59@gmail.com>
2026-08-09 22:33:34 +0545

    Update workshop date and registration links

--- lib/workshop.ts
+++ lib/workshop.ts
@@ -1,18 +1,18 @@
 export const workshop = {
   logoPath: "/sen-logo.png",
-  date: "9th August",
+  date: "12th August",
   time: "8 PM",
   duration: "90 minutes",
   vimeoVideoUrl: "https://player.vimeo.com/video/1203762169",
-  whatsappGroupLink: "https://chat.whatsapp.com/KRoCcuBPhDE1ri9rCUdLmZ",
+  whatsappGroupLink: "https://chat.whatsapp.com/JJ6ojWO28bh5lhhv0Sk61V",
 };
 
 export const campaignZoomLinks = {
-  email: "https://us06web.zoom.us/webinar/register/3817859974841/WN_SuE-0_qrTUKH-14WLeXRYg",
-  whatsapp: "https://us06web.zoom.us/webinar/register/2717859974895/WN_SuE-0_qrTUKH-14WLeXRYg",
-  "workshop-workflow": "https://us06web.zoom.us/webinar/register/2617859974974/WN_SuE-0_qrTUKH-14WLeXRYg",
-  instagram: "https://us06web.zoom.us/webinar/register/3117859975033/WN_SuE-0_qrTUKH-14WLeXRYg",
-  facebook: "https://us06web.zoom.us/webinar/register/7017859974935/WN_SuE-0_qrTUKH-14WLeXRYg",
-  "facebook-group": "https://us06web.zoom.us/webinar/register/4017859976179/WN_SuE-0_qrTUKH-14WLeXRYg",
-  tiktok: "https://us06web.zoom.us/webinar/register/1017859976350/WN_SuE-0_qrTUKH-14WLeXRYg",
+  email: "https://us06web.zoom.us/webinar/register/8817859980259/WN_acFDPyOJSQutNwseW3GJaA",
+  whatsapp: "https://us06web.zoom.us/webinar/register/1317859980328/WN_acFDPyOJSQutNwseW3GJaA",
+  "workshop-workflow": "https://us06web.zoom.us/webinar/register/2617859980467/WN_acFDPyOJSQutNwseW3GJaA",
+  instagram: "https://us06web.zoom.us/webinar/register/4117859980408/WN_acFDPyOJSQutNwseW3GJaA",
+  facebook: "https://us06web.zoom.us/webinar/register/7617859980366/WN_acFDPyOJSQutNwseW3GJaA",
+  "facebook-group": "https://us06web.zoom.us/webinar/register/7617859980366/WN_acFDPyOJSQutNwseW3GJaA",
+  tiktok: "https://us06web.zoom.us/webinar/register/7517859980555/WN_acFDPyOJSQutNwseW3GJaA",
 } as const;
